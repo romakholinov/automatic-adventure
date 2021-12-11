fe7bf3cd8dd431860c342892757da38e74213048
romakholinov <rakholinov@edu.hse.ru>
2021-12-11 11:31:41 +0300

    Add comment

--- main.py
+++ main.py
@@ -7,43 +7,69 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def index():
+    # hello
     return render_template("index.html")
+
 
 @app.route('/cakesLine')
 def cakesLine():
     return render_template("cakesLineGraph.html")
+
+
 @app.route('/piesLine')
 def piesLine():
     return render_template("piesLineGraph.html")
+
+
 @app.route('/cookiesLine')
 def cookiesLine():
     return render_template("cookiesLineGraph.html")
+
+
 @app.route('/smoothiesLine')
 def smoothiesLine():
     return render_template("smoothiesLineGraph.html")
+
+
 @app.route('/coffeeLine')
 def coffeeLine():
     return render_template("coffeeLineGraph.html")
+
+
 @app.route('/cakesBar')
 def cakesBar():
     return render_template("cakesBarGraph.html")
+
+
 @app.route('/piesBar')
 def piesBar():
     return render_template("piesBarGraph.html")
+
+
 @app.route('/cookiesBar')
 def cookiesBar():
     return render_template("cookiesBarGraph.html")
+
+
 @app.route('/smoothiesBar')
 def smoothiesBar():
     return render_template("smoothiesBarGraph.html")
+
+
 @app.route('/coffeeBar')
 def coffeeBar():
     return render_template("coffeeBarGraph.html")
+
+
 @app.route('/foodComparison')
 def foodComparison():
     return render_template("foodComparisonGraph.html")
+
+
 @app.route('/drinksComparison')
 def drinksComparison():
     return render_template("drinksComparisonGraph.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
